7673d97ef2d96c284ada91a7ebfac056f4f39c5b
LucasFavarao <124338066+LucasFavarao@users.noreply.github.com>
2024-04-03 15:28:38 -0400

    Add files via upload
    Updated serverTest.py. You should now be able to open a server and open a webpage and play the audio. Still need to find out how to receive requests.

--- serverTest.py
+++ serverTest.py
@@ -1,10 +1,49 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
+import mimetypes
 
-def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('', 8000)
-    httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
+hostName = "localhost"
+serverPort = 8000
 
-#issue is with handler class in function. Need to find a value to pass. 
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Parse the requested file path
+        if self.path == '/':
+            self.path = '/index.html'
 
-run()
+        try:
+            file_extension = os.path.splitext(self.path)[1]
+            if file_extension == '.mp3':
+                # Set the appropriate Content-Type header for audio files
+                self.send_response(200)
+                self.send_header('Content-type', mimetypes.types_map[file_extension])
+                self.end_headers()
+
+                # Open and send the audio file
+                with open(self.path[1:], 'rb') as file:
+                    self.wfile.write(file.read())
+            else:
+                # For other file types, send the file content as text
+                with open(self.path[1:], 'r') as file:
+                    file_to_open = file.read()
+                    self.send_response(200)
+                    self.end_headers()
+                    self.wfile.write(bytes(file_to_open, 'utf-8'))
+        except Exception as e:
+            file_to_open = "File not found"
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(bytes(file_to_open, 'utf-8'))
+            print(f"Error: {e}")
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
